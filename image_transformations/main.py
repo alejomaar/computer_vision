a57a3b8b7654354b8b732b0df77bc99d686eaf87
alejomaar <demonmaar@gmail.com>
2022-10-03 23:31:13 -0500

    DOC documenting the code and reorder

--- image_transformations/main.py
+++ image_transformations/main.py
@@ -1,5 +1,5 @@
 # @author: Manuel Alejandro Aponte
-# Program to find difference between two images
+# Undistort images through perspective transformation
 
 #Intersting resources:
 #https://medium.com/analytics-vidhya/opencv-perspective-transformation-9edffefb2143
@@ -7,34 +7,38 @@
 import cv2
 import numpy as np
 
-pts_source=[]
-
+#Points selected by user 
+pts_source=[] #Xcoordinate,Ycoordinate
 
 # Load image
 image = cv2.imread('cards.jpg')
-#image = cv2.resize(image, (400,600))
+
+# Preserve the original image
 original_img= image.copy()
 
 
-def draw_rectangle(action, x, y, flags, *userdata):
-  # Referencing global variables 
+def click_event(action, x, y, flags, *userdata):
+    # Referencing global variables 
     global pts_source,image
 
     if action != cv2.EVENT_LBUTTONDOWN:
         return
     len_corners=  len(pts_source)
     point = [x,y]  
+    
+    #Draw new point in screen
     if(len_corners<3):            
         pts_source.append(point)
         draw_point(image,x,y)
+        
+    #Draw undistorted perspective  
     if(len_corners==3):
         pts_source.append(point)
         draw_point(image,x,y)
-        print('Execute')   
         image2 = perspective_transform(original_img,np.float32(pts_source))
         cv2.imshow("window2", image2)
-        #image = np.concatenate((original_img, undistorted_img), axis=1) 
-         
+
+    #Clear all points     
     if(len_corners==4):
         pts_source=[]
         image= original_img.copy()
@@ -43,7 +47,7 @@ def draw_rectangle(action, x, y, flags, *userdata):
     
     
 def draw_point(img,x,y):
-    cv2.circle(img, (x,y), radius=5, color=(255, 0, 0), thickness=-1)
+    cv2.circle(img, (x,y), radius=3, color=(200, 100, 0), thickness=-1)
     
 def get_dimensions():
     maxHeight= 712
@@ -59,15 +63,16 @@ def perspective_transform(img,pts_source):
     transform_img = cv2.warpPerspective(img,M,(maxWidth,maxHeight))
     return transform_img
 
+
+if __name__=='__main__':
+    #Set configuration
+    cv2.namedWindow("window")
+    cv2.setMouseCallback("window", click_event,image)
     
-cv2.namedWindow("window")
-cv2.setMouseCallback("window", draw_rectangle,image)
+    #While for interactive mode
+    while True:
 
-
-
-while True:
-    # both windows are displaying the same img
-    cv2.imshow("window", image)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
-cv2.destroyAllWindows()
+        cv2.imshow("window", image)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    cv2.destroyAllWindows()
