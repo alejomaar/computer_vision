9a7315cd8ad273ce55311e167aa7192733fc2f98
alejomaar <demonmaar@gmail.com>
2022-10-03 23:19:23 -0500

    FIX fixed dimensions

--- image_transformations/main.py
+++ image_transformations/main.py
@@ -1,6 +1,8 @@
 # @author: Manuel Alejandro Aponte
 # Program to find difference between two images
 
+#Intersting resources:
+#https://medium.com/analytics-vidhya/opencv-perspective-transformation-9edffefb2143
 
 import cv2
 import numpy as np
@@ -43,34 +45,15 @@ def draw_rectangle(action, x, y, flags, *userdata):
 def draw_point(img,x,y):
     cv2.circle(img, (x,y), radius=5, color=(255, 0, 0), thickness=-1)
     
-def get_perspective_dim(pts_source):
-    Ax,Ay = pts_source[0]
-    Bx,By = pts_source[1]
-    Cx,Cy = pts_source[2]
-    Dx,Dy = pts_source[3]   
-    
-    width_AD = np.sqrt(((Ax - Dx) ** 2) + ((Ay - Dy) ** 2))
-    width_BC = np.sqrt(((Bx - Cx) ** 2) + ((Bx - Cx) ** 2))
-    maxWidth = max(int(width_AD), int(width_BC))
-
-
-    height_AB = np.sqrt(((Ax - Bx) ** 2) + ((Ay - By) ** 2))
-    height_CD = np.sqrt(((Cx - Dx) ** 2) + ((Cy - Dy) ** 2))
-    
-    maxHeight = max(int(height_AB), int(height_CD))
-    
-    print(pts_source)
-    print()
-    print(maxWidth,maxHeight)
-    
-    maxHeight= 890
-    maxWidth = 640
+def get_dimensions():
+    maxHeight= 712
+    maxWidth = 512
     return np.float32([[0, 0], [0, maxHeight - 1], [maxWidth - 1, maxHeight - 1],[maxWidth - 1, 0]]),maxWidth,maxHeight
     
     
 def perspective_transform(img,pts_source):
     
-    pts_destination,maxWidth,maxHeight= get_perspective_dim(pts_source)    
+    pts_destination,maxWidth,maxHeight= get_dimensions()    
     M = cv2.getPerspectiveTransform(pts_source,pts_destination)
         
     transform_img = cv2.warpPerspective(img,M,(maxWidth,maxHeight))
@@ -80,9 +63,6 @@ def perspective_transform(img,pts_source):
 cv2.namedWindow("window")
 cv2.setMouseCallback("window", draw_rectangle,image)
 
-
-#transform = np.array([[1,0,0],[1,0,0],[1,0,0]])
-#img_transform = cv2.warpPerspective(img, transform)
 
 
 while True:
